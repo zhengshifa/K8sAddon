415748e7ee83ce33df687a21fbae99ab1270868c
zhengshifa <zhengshifa@139.com>
2024-11-10 07:59:41 +0800

    自动提交 2024-11-10_07:59:41

--- tasks/lib.sh
+++ tasks/lib.sh
@@ -13,21 +13,18 @@ fn_log_error() { TIMESTAMP=$(date +'%Y-%m-%d %H:%M:%S'); echo -e "$TIMESTAMP \03
 # j2模板文件渲染
 fn_j2_to_files() {
 
-    if ! which jinja2;then
+    if ! which jinja2 &>/dev/null;then
         pip install jinja2-cli jinja2
     fi
 
     for values in ${base_dir}/templates/${filename}/*.j2; do
         set -a
-        #. ${base_dir}/vars/${env_var}/${filename}
         j2_name=$(basename "$values")
-       # envsubst < ${values} > ${values%%.*}-${env_var}.yaml
         manifests_dir=${base_dir}/manifests/${env_var}/${filename}
         if [ ! -d "$manifests_dir" ];then
             mkdir -p $manifests_dir
         fi
-        #envsubst < ${values} > ${manifests_dir}/${j2_name%.*}
-        jinja2 ${values}  ${base_dir}/vars/${env_var}/${filename} --format=yaml  -o ${manifests_dir}/${j2_name%.*}
+        jinja2 ${values} ${base_dir}/vars/${env_var}/${filename} --format=yaml  -o ${manifests_dir}/${j2_name%.*}
         [ $? = '0' ] && fn_log_info "渲染成功,生成文件 ${manifests_dir}/${j2_name%.*}"
     done
 
